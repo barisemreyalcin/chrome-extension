8a68139fd42d6c000342f910ddb71e66040bea09
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-07-10 17:00:27 +0300

    js: refactoring renderLeads() and create an alert for empty lead

--- app.js
+++ app.js
@@ -8,30 +8,34 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
 
-btnSave.addEventListener("click", function() {
-    myLeads.push(input.value);
-    input.value = "";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
-})
-
-btnDelete.addEventListener("dblclick", function() {
-    localStorage.clear();
-    myLeads = [];
-    renderLeads();
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    for(let i = 0; i < myLeads.length; i++) {
+    for(let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-        <a href="${myLeads[i]}" target="_blank"><i class="fa-solid fa-link"></i>${myLeads[i]}</a>
+        <a href="${leads[i]}" target="_blank"><i class="fa-solid fa-link"></i>${leads[i]}</a>
         </li>
         ` 
     }
     ul.innerHTML = listItems;
 }
+
+btnSave.addEventListener("click", function() {
+    if(input.value == "") {
+        alert("Please, enter a lead.");
+    } else {
+        myLeads.push(input.value);
+        input.value = "";
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    }
+})
+
+btnDelete.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
